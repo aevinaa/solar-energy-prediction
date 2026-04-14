0b3dfcd18c9cfe9ebe6c0d67323f32c21d1c9739
aevinaa <agrawaladiti247@gmail.com>
2026-04-15 05:05:45 +0530

    connect frontend to live frontend

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
     }
     try {
       if (mode === "instant") {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("http:///predict", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
