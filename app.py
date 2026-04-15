f1bbd6283e64e25bfdb517fb0c63c0540f7fd6db
aevinaa <agrawaladiti247@gmail.com>
2026-04-15 12:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def init_db():
     cursor=conn.cursor()
 
     cursor.execute(""" CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT UNIQUE, password TEXT)""")
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS predictions(id INTEGER PRIMARY KEY AUTOINCREMENT, irradiation REAL, ambient_temperature REAL, module_temperature REAL, hour INTEGER, predicted_power REAL, date TEXT)""")
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS predictions(id INTEGER PRIMARY KEY AUTOINCREMENT,user_id INTEGER, irradiation REAL, ambient_temperature REAL, module_temperature REAL, hour INTEGER, predicted_power REAL, date TEXT)""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS forecasts (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, location TEXT, date TEXT, predicted_power REAL)""")
     conn.commit()
     conn.close()
