c0d3aff31cdcd05bc7deed9c946242c9841a564b
aevinaa <agrawaladiti247@gmail.com>
2026-04-15 12:49:04 +0530

    changed link name

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://solar-energy-prediction-git-main-aevinaas-projects.vercel.app"],  
+    allow_origins=["https://solar-energy-prediction-psi.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
